ba7018ca5a8639f38070cbebd5d22e7e5445912f
Mastro-Gibbs <stefano.fattore.official@gmail.com>
2021-03-29 12:43:50 +0200

    improved efficiency, improved dynamic charts drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from machine import I2C, Pin
 from bme280 import BME280
-
-import time
+from time import time, sleep
 import framebuf
 
 import oled
@@ -13,43 +12,34 @@ import filemanager
 i2c0 = I2C(0, scl = Pin(1), sda = Pin(0), freq = 400000)
 i2c1 = I2C(1, scl = Pin(3), sda = Pin(2), freq = 400000)
 
-#OLED'S WIDTH AND HEIGTH
-WIDTH  = 128
-HEIGTH = 64
-
 #OLED OBJS INITS
-oled0 = oled.Charts_and_Images(WIDTH, HEIGTH, i2c0)
-oled1 = oled.Snapshot(WIDTH, HEIGTH, i2c1)
+oled0 = oled.Charts_and_Images(128, 64, i2c0)
+oled1 = oled.Snapshot(128, 64, i2c1)
 
 #BME280 SENSOR OBJ INIT
 sensor = BME280(i2c = i2c0)
 
-#DATA TUPLES FOR CHARTS "(FILE_PATH, MIN_XCOORD, MAX_XCOORD)"
-tdata = ("data/temperature", 18, 128)
-hdata = ("data/humidity", 26, 128)
-pdata = ("data/pressure", 34, 128)
-
 #FILES MANAGER OBJS INIT 
-tfile = filemanager.Filemanager(tdata[0])
-hfile = filemanager.Filemanager(hdata[0])
-pfile = filemanager.Filemanager(pdata[0])
+temperatureFile = filemanager.Filemanager("data/temperature")
+humidityFile    = filemanager.Filemanager("data/humidity")
+pressureFile    = filemanager.Filemanager("data/pressure")
 
 #LOGO IMAGE
 raspy_logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
-raspi = framebuf.FrameBuffer(raspy_logo, 32, 32, framebuf.MONO_HLSB)
+raspi      = framebuf.FrameBuffer(raspy_logo, 32, 32, framebuf.MONO_HLSB)
 
 
 #*********LOGO FUNCTION*********#
 
 #Print on oled1 infos, on oled0 a bouncing rasperry logo about 7 seconds
 def logo():
+    oled0.frame(0, 0, 128, 64, 1)
     oled1.frame(0, 0, 128, 64, 1)
-    oled1.info("Pico Weather", "BOOTING", "(maybe)")
+    oled1.threewords("Pico Weather", "BOOTING", "(maybe)")
     dx = 1
     dy = 1
     x = 1
     y = 1
-    oled0.frame(0, 0, 128, 64, 1)
     for loop in range(705):
         oled0.image(raspi, x, y)
         x = x + dx
@@ -79,84 +69,75 @@ def sensor_snapshot():
 
 #These three functions below create the graph and light up the pixel relative to the past value (ycoord) and x coordinate.
 #The first parameter is the content of the file, to recreate the previously deleted graph.
-def temperature_chart(tfiledata, temp, xcoord):
-    oled0.temp_chart(tfiledata)
-    oled0.turnon_pixel("temperature", int(temp), xcoord)
+def temperature_chart(tfile, temp, xcoord):
+    oled0.temp_chart(tfile.read())
+    oled0.turnon_pixel("temperature", int(temp), xcoord, tfile)
 
-def humidity_chart(hfiledata, hum, xcoord):
-    oled0.hum_chart(hfiledata)
-    oled0.turnon_pixel("humidity", int(hum), xcoord)
+def humidity_chart(hfile, hum, xcoord):
+    oled0.hum_chart(hfile.read())
+    oled0.turnon_pixel("humidity", int(hum), xcoord, hfile)
     
-def pressure_chart(pfiledata, press, xcoord):
-    oled0.press_chart(pfiledata)
-    oled0.turnon_pixel("pressure", int(press), xcoord)
-  
+def pressure_chart(pfile, press, xcoord):
+    oled0.press_chart(pfile.read())
+    oled0.turnon_pixel("pressure", int(press), xcoord, pfile)
+    
 
 #************MAIN***********#  
   
 def main():
-    tfile.erase()
-    pfile.erase()
-    hfile.erase()
+    temperatureFile.erase()
+    humidityFile.erase()
+    pressureFile.erase()
     
-    sec0 = time.time() #get the time
+    sec0 = time() #get the time
     
     ticket = 1
     mutex = [True, False, False] #like an array of semaphores for mutex
     
     #min xcoord for each chart
-    t_xcoord = tdata[1]
-    p_xcoord = pdata[1]
-    h_xcoord = hdata[1]
-    
-    #obj for store the content of the files
-    pfiledata = []
+    t_xcoord = 18
+    h_xcoord = 26
+    p_xcoord = 34
     
     while True:
-        sec1 = time.time() #reget the time
+        sec1 = time() #reget the time
         
         #print shapshots and return values
         temp_value, hum_value, press_value = sensor_snapshot()
         
-        #If 50 seconds have elapsed, change the value of the next element to the first True of the semaphore array
-        if (sec1 - sec0) >= 55:
+        #If 120 seconds have elapsed, change the value of the next element to the first True of the semaphore array
+        if (sec1 - sec0) >= 10:
             sec0 = sec1
             ticket = ticket % 3
             mutex[ticket] = True
             ticket += 1
         
         if mutex[0]:
-            #If there are records, it prints the relative image of the previous graph
-            if pfiledata:
-                oled0.pressure_image(pfiledata[-1])
-                
-            mutex[0] = False
-            tfiledata = tfile.read()
-            temperature_chart(tfiledata, temp_value, t_xcoord)
+            if t_xcoord > 128:
+                t_xcoord = 18
+                temperatureFile.erase()
+            temperature_chart(temperatureFile, temp_value, t_xcoord)
             t_xcoord += 1
-            if t_xcoord == tdata[2]:
-                t_xcoord = tdata[1]
-                tfile.erase()
+            mutex[0] = False
         
         if mutex[1]:
-            mutex[1] = False
-            hfiledata = hfile.read()
-            humidity_chart(hfiledata, hum_value, h_xcoord)
+            if h_xcoord > 128:
+                h_xcoord = 26
+                humidityFile.erase()
+            humidity_chart(humidityFile, hum_value, h_xcoord)
             h_xcoord += 1
-            if h_xcoord == hdata[2]:
-                h_xcoord = hdata[1]
-                hfile.erase()
+            mutex[1] = False
             
         if mutex[2]:
-            mutex[2] = False
-            pfiledata = pfile.read()
-            pressure_chart(pfiledata, press_value, p_xcoord)
+            if p_xcoord > 128:
+                p_xcoord = 34
+                pressureFile.erase()
+            oled0.pressure_image(press_value)
+            pressure_chart(pressureFile, press_value, p_xcoord)
             p_xcoord += 1
-            if p_xcoord == pdata[2]:
-                p_xcoord = pdata[1]
-                pfile.erase()
+            mutex[2] = False
         
-        time.sleep(5)
+        sleep(10)
 
 logo()
 app_name()
